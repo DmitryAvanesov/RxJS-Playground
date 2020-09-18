7f95b623fb40e3ee48f5cafc2b890c0e6b346661
Dima <38003563+DmitryAvanesov@users.noreply.github.com>
2020-09-18 14:48:20 +0700

    Finished the app + updated readme

--- src/app/alphabet-invasion-game/alphabet-invasion-game.component.ts
+++ src/app/alphabet-invasion-game/alphabet-invasion-game.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, fromEvent, interval } from 'rxjs';
-import { map, scan, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, fromEvent, interval, Subject } from 'rxjs';
+import { map, scan, skip, switchMap, takeUntil } from 'rxjs/operators';
 import { Letter, State } from './types';
 
 @Component({
@@ -15,13 +15,17 @@ export class AlphabetInvasionGameComponent implements OnInit {
     letters: [],
   };
   spaceBetweenLetters = 50;
-  private numberOfRows = 12;
+  numberOfRows = 12;
+  gameOverText = 'Game over';
+  gameOverSubject$ = new BehaviorSubject(false);
+
   private numberOfCols = 8;
-  private levelUpEvery = 20;
-  private intervalSubject = new BehaviorSubject(1000);
+  private levelUpEvery = 25;
+  private intervalSubject$ = new BehaviorSubject(750);
 
   private keys$ = fromEvent(document, 'keydown').pipe(
-    map((e: KeyboardEvent) => e.key)
+    map((e: KeyboardEvent) => e.key),
+    takeUntil(this.gameOverSubject$.pipe(skip(1)))
   );
 
   private randomLetter = () => {
@@ -29,8 +33,9 @@ export class AlphabetInvasionGameComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    const letters$ = this.intervalSubject.pipe(
-      switchMap((period: number) => interval(period))
+    const letters$ = this.intervalSubject$.pipe(
+      switchMap((period: number) => interval(period)),
+      takeUntil(this.gameOverSubject$.pipe(skip(1)))
     );
 
     letters$.subscribe((_time: number) => {
@@ -43,17 +48,27 @@ export class AlphabetInvasionGameComponent implements OnInit {
         row: 0,
         col: Math.floor(Math.random() * this.numberOfCols),
       });
+
+      if (this.state.letters[0].row === this.numberOfRows) {
+        this.gameOverSubject$.next(true);
+      }
     });
 
     this.keys$.subscribe((key: string) => {
-      if (this.state.letters[0].symbol === key) {
+      if (this.state.letters.length && this.state.letters[0].symbol === key) {
         this.state.letters.shift();
         this.state.score++;
-      }
 
-      if (this.state.score > 0 && this.state.score % this.levelUpEvery === 0) {
-        this.state.letters = [];
-        this.state.level++;
+        if (
+          this.state.score > 0 &&
+          this.state.score % this.levelUpEvery === 0
+        ) {
+          this.state.letters = [];
+          this.state.level++;
+          this.intervalSubject$.next(
+            Math.max(50, this.intervalSubject$.value - 50)
+          );
+        }
       }
     });
   }
